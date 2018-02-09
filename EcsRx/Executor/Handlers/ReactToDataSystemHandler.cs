fb71a97f7ec04899b40c2a82939b9a3405b63926
LP <grofit1@yahoo.com>
2018-02-09 15:28:20 +0000

    Updated more systems.

--- EcsRx/Executor/Handlers/ReactToDataSystemHandler.cs
+++ EcsRx/Executor/Handlers/ReactToDataSystemHandler.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reflection;
 using EcsRx.Entities;
 using EcsRx.Extensions;
 using EcsRx.Groups;
@@ -9,61 +12,101 @@ using EcsRx.Systems;
 
 namespace EcsRx.Executor.Handlers
 {
-    public class ReactToDataSystemHandler : IReactToDataSystemHandler
+    public class ReactToDataSystemHandler : IConventionalSystemHandler<ISystem>
     {
-        public IPoolManager PoolManager { get; }
+        private IDictionary<ISystem, IDisposable> _subscriptions;
+        private IDictionary<ISystem, IDictionary<Guid, IDisposable>> _systemSubscriptions;
+        private MethodInfo _processEntityMethod;
 
+        public IPoolManager PoolManager { get; }
+        
         public ReactToDataSystemHandler(IPoolManager poolManager)
         {
             PoolManager = poolManager;
+            _subscriptions = new Dictionary<ISystem, IDisposable>();
+            _systemSubscriptions = new Dictionary<ISystem, IDictionary<Guid, IDisposable>>();
+            _processEntityMethod = GetType().GetMethod("ProcessEntity");
         }
 
         // TODO: This is REALLY bad but currently no other way around the dynamic invocation lookup stuff
-        public IEnumerable<SubscriptionToken> SetupWithoutType(ISystem system)
+        public Func<IEntity, IDisposable> CreateEntityProcessorFunction(ISystem system)
         {
-            var method = GetType().GetMethod("Setup");
             var genericDataType = system.GetGenericDataType();
-            var genericMethod = method.MakeGenericMethod(genericDataType);
-            return (IEnumerable<SubscriptionToken>)genericMethod.Invoke(this, new[] { system });
+            var genericMethod = _processEntityMethod.MakeGenericMethod(genericDataType);
+            return entity => (IDisposable) genericMethod.Invoke(this, new object[] {system, entity});
         }
 
-        // TODO: This is REALLY bad but currently no other way around the dynamic invocation lookup stuff
-        public SubscriptionToken ProcessEntityWithoutType(ISystem system, IEntity entity)
-        {
-            var method = GetType().GetMethod("ProcessEntity");
-            var genericDataType = system.GetGenericDataType();
-            var genericMethod = method.MakeGenericMethod(genericDataType);
-            return (SubscriptionToken)genericMethod.Invoke(this, new object[] { system, entity });
-        }
-
-        public IEnumerable<SubscriptionToken> Setup<T>(IReactToDataSystem<T> system)
-        {
-            var groupAccessor = PoolManager.CreateObservableGroup(system.TargetGroup);
-            return groupAccessor.Entities.Select(x => ProcessEntity(system, x));
-        }
-
-        public SubscriptionToken ProcessEntity<T>(IReactToDataSystem<T> system, IEntity entity)
+        public IDisposable ProcessEntity<T>(IReactToDataSystem<T> system, IEntity entity)
         {
             var hasEntityPredicate = system.TargetGroup is IHasPredicate;
-            var subscription = system.ReactToData(entity)
-                    .Subscribe(x =>
-                    {
-                        if (hasEntityPredicate)
-                        {
-                            var groupPredicate = system.TargetGroup as IHasPredicate;
-                            if(groupPredicate.CanProcessEntity(entity))
-                            { system.Execute(entity, x); }
-                            return;
-                        }
-
-                        system.Execute(entity, x);
-                    });
-
-            return new SubscriptionToken(entity, subscription);
+            var reactObservable = system.ReactToData(entity);
+            
+            if (!hasEntityPredicate)
+            { return reactObservable.Subscribe(x => system.Execute(entity, x)); }
+            
+            var groupPredicate = system.TargetGroup as IHasPredicate;
+            return reactObservable
+                .Where(x => groupPredicate.CanProcessEntity(entity))
+                .Subscribe(x => system.Execute(entity, x));
         }
 
+        public bool CanHandleSystem(ISystem system)
+        { return system.IsReactiveDataSystem(); }
+
+        public void SetupSystem(ISystem system)
+        {
+            var processEntityFunction = CreateEntityProcessorFunction(system);
+
+            var entityChangeSubscriptions = new CompositeDisposable();
+            _subscriptions.Add(system, entityChangeSubscriptions);
+            
+            var entitySubscriptions = new Dictionary<Guid, IDisposable>();
+            _systemSubscriptions.Add(system, entitySubscriptions);
+            
+            var groupAccessor = PoolManager.CreateObservableGroup(system.TargetGroup);
+
+            groupAccessor.OnEntityAdded
+                .Subscribe(x =>
+                {
+                    var subscription = processEntityFunction(x);
+                    entitySubscriptions.Add(x.Id, subscription);
+                })
+                .AddTo(entityChangeSubscriptions);
+            
+            groupAccessor.OnEntityRemoved
+                .Subscribe(x =>
+                {
+                    entitySubscriptions.RemoveAndDispose(x.Id);
+                })
+                .AddTo(entityChangeSubscriptions);
+            
+            
+            foreach (var entity in groupAccessor.Entities)
+            {
+                var subscription = processEntityFunction(entity);
+                entitySubscriptions.Add(entity.Id, subscription);
+            }
+        }
+
+        public void DestroySystem(ISystem system)
+        {
+            _subscriptions.RemoveAndDispose(system);
+            
+            var entitySubscriptions = _systemSubscriptions[system];
+            entitySubscriptions.Values.DisposeAll();
+            entitySubscriptions.Clear();
+            _systemSubscriptions.Remove(system);
+        }
+        
         public void Dispose()
         {
+            _subscriptions.DisposeAll();
+            foreach (var entitySubscriptions in _systemSubscriptions.Values)
+            {
+                entitySubscriptions.Values.DisposeAll();
+                entitySubscriptions.Clear();
+            }
+            _systemSubscriptions.Clear();
         }
     }
 }

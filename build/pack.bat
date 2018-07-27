5bfde745c0f1ab6d16120911bcf5d77d7bc757fb
LP (BB) <lee.prosser@embersoft.co.uk>
2018-07-27 15:24:38 +0100

    Its a breaking change so bumping to 0.3.0

--- build/pack.bat
+++ build/pack.bat
@@ -1,4 +1,4 @@
-set version=0.2.1
+set version=0.3.0
 dotnet pack ../src/EcsRx -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/EcsRx.Systems -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/EcsRx.Views -c Release -o ../../_dist /p:version=%version%

e1c209d884609819776514727f8de1ff4163c180
LP (BB) <lee.prosser@embersoft.co.uk>
2018-08-23 13:55:56 +0100

    Updated pack

--- build/pack.bat
+++ build/pack.bat
@@ -1,5 +1,8 @@
-set version=0.3.0
+set version=1.0.0
+dotnet pack ../src/EcsRx.MicroRx -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/EcsRx -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/EcsRx.Systems -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/EcsRx.Views -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/EcsRx.Infrastructure -c Release -o ../../_dist /p:version=%version%
+dotnet pack ../src/EcsRx.Infrastructure.Ninject -c Release -o ../../_dist /p:version=%version%
+dotnet pack ../src/EcsRx.ReactiveData -c Release -o ../../_dist /p:version=%version%

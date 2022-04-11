b7d76e98905aa90494b0639d1f7940a97024ed30
LP <grofit1@yahoo.com>
2022-04-11 13:08:47 +0100

    Moved examples over to use Spectre

--- src/EcsRx.Examples/Program.cs
+++ src/EcsRx.Examples/Program.cs
@@ -9,41 +9,63 @@ using EcsRx.Examples.ExampleApps.LoadingEntityDatabase;
 using EcsRx.Examples.ExampleApps.Performance;
 using EcsRx.Examples.ExampleApps.Playground.ClassBased;
 using EcsRx.Examples.ExampleApps.Playground.StructBased;
+using Spectre.Console;
 
 namespace EcsRx.Examples
 {
     class Program
     {
+        class Example
+        {
+            public string Name { get; }
+            public Action Executor { get; }
+
+            public Example(string name, Action executor)
+            {
+                Name = name;
+                Executor = executor;
+            }
+        }
+        
         static void Main(string[] args)
         {
-            // Sample examples
-            new HelloWorldExampleApplication().StartApplication();
-            //new ComputedGroupExampleApplication().StartApplication();
-            //new HealthExampleApplication().StartApplication();
-            //new PersistDataApplication().StartApplication();
-            //new LoadingEntityDatabaseApplication().StartApplication();
-            //new BatchedGroupExampleApplication().StartApplication();
-            //new SetupSystemPriorityApplication().StartApplication();
+            var availableExamples = new []
+            {
+                new Example("Scenario: Hello World", () => new HelloWorldExampleApplication().StartApplication()),
+                new Example("Scenario: Computed Groups", () => new ComputedGroupExampleApplication().StartApplication()),
+                new Example("Scenario: Health Deduction", () => new HealthExampleApplication().StartApplication()),
+                new Example("Scenario: Data Persistence", () => new PersistDataApplication().StartApplication()),
+                new Example("Scenario: Entity Databases", () => new LoadingEntityDatabaseApplication().StartApplication()),
+                new Example("Scenario: Batched Groups", () => new BatchedGroupExampleApplication().StartApplication()),
+                new Example("Scenario: System Priorities", () => new SetupSystemPriorityApplication().StartApplication()),
+                
+                new Example("Performance: Systems", () => new SimpleSystemApplication().StartApplication()),
+                new Example("Performance: Default Entity", () => new EntityPerformanceApplication().StartApplication()),
+                new Example("Performance: Optimised Entity", () => new OptimizedEntityPerformanceApplication().StartApplication()),
+                new Example("Performance: Default Group", () => new GroupPerformanceApplication().StartApplication()),
+                new Example("Performance: Optimised Group", () => new OptimizedGroupPerformanceApplication().StartApplication()),
+                new Example("Performance: Entity Creation", () => new MakingLotsOfEntitiesApplication().StartApplication()),
+                
+                new Example("Dev: Using Classes 1", () => new Class1Application().StartApplication()),
+                new Example("Dev: Using Classes 2", () => new Class2Application().StartApplication()),
+                new Example("Dev: Using Classes 3", () => new Class3Application().StartApplication()),
+                new Example("Dev: Using Classes 4", () => new Class4Application().StartApplication()),
+                new Example("Dev: Using Structs 1", () => new Struct1Application().StartApplication()),
+                new Example("Dev: Using Structs 2", () => new Struct2Application().StartApplication()),
+                new Example("Dev: Using Structs 3", () => new Struct3Application().StartApplication()),
+                new Example("Dev: Using Structs 4", () => new Struct4Application().StartApplication()),
+                new Example("Dev: Using Structs 4", () => new Struct4BApplication().StartApplication())
+            };
 
-            // Performance examples
-            //new SimpleSystemApplication().StartApplication();
-            //new GroupPerformanceApplication().StartApplication();
-            //new OptimizedGroupPerformanceApplication().StartApplication();
-            //new OptimizedEntityPerformanceApplication().StartApplication();
-            //new EntityPerformanceApplication().StartApplication();
-            //new MakingLotsOfEntitiesApplication().StartApplication();
-           
-            // Playground examples
-            //new Class1Application().StartApplication();
-            //new Struct1Application().StartApplication();
-            //new Class2Application().StartApplication();
-            //new Struct2Application().StartApplication();
-            //new Class3Application().StartApplication();
-            //new Struct3Application().StartApplication();
-            //new Class4Application().StartApplication();
-            //new Struct4Application().StartApplication();            
-            //new Struct4BApplication().StartApplication();
-
+            var exampleSelector = new SelectionPrompt<Example>()
+                    .Title("Welcome To The [green]EcsRx[/] examples, what example do you want to run?")
+                    .PageSize(50)
+                    .MoreChoicesText("[grey](Move up and down to reveal more examples)[/]")
+                    .UseConverter(x => x.Name)
+                    .AddChoices(availableExamples);
+            
+            var exampleToRun = AnsiConsole.Prompt(exampleSelector);
+            exampleToRun.Executor();
         }
     }
 }

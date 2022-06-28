4f8402a34574bf6c923b70f9c59f28446fdf0806
Jonas Fischer <fijo.com@googlemail.com>
2022-06-28 18:11:56 +0200

    fixed unintended breaking changes with groups
    after updating from 5.0.110 to 6.0.131

--- src/EcsRx/Groups/Group.cs
+++ src/EcsRx/Groups/Group.cs
@@ -14,13 +14,34 @@ namespace EcsRx.Groups
 		/// <summary>
 		/// Hint: maybe consider using Group.Empty for better performance, unless you plan on changing the group afterwards.
 		/// </summary>
-		public Group() {}
+		public Group() {
+			RequiredComponents = Array.Empty<Type>();
+			ExcludedComponents = Array.Empty<Type>();
+		}
 
-		public Group(params Type[] requiredComponents) : this(requiredComponents, Array.Empty<Type>()) {}
+		public Group(params Type[] requiredComponents) {
+            if (requiredComponents is null)
+            {
+                throw new ArgumentNullException(nameof(requiredComponents));
+            }
+
+            RequiredComponents = requiredComponents.ToArray();
+			ExcludedComponents = Array.Empty<Type>();
+		}
         
 	    public Group(IEnumerable<Type> requiredComponents, IEnumerable<Type> excludedComponents)
 	    {
-		    RequiredComponents = requiredComponents.ToArray();
+            if (requiredComponents is null)
+            {
+                throw new ArgumentNullException(nameof(requiredComponents));
+            }
+
+            if (excludedComponents is null)
+            {
+                throw new ArgumentNullException(nameof(excludedComponents));
+            }
+
+            RequiredComponents = requiredComponents.ToArray();
 		    ExcludedComponents = excludedComponents.ToArray();
 	    }
     }

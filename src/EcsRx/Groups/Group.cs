e54592cad621022c9bfb7ad11574e84d443eb466
Jonas Fischer <fijo.com@googlemail.com>
2022-07-04 17:43:04 +0200

    graceful null handling for Group

--- src/EcsRx/Groups/Group.cs
+++ src/EcsRx/Groups/Group.cs
@@ -14,29 +14,22 @@ namespace EcsRx.Groups
 		/// <summary>
 		/// Hint: maybe consider using Group.Empty for better performance, unless you plan on changing the group afterwards.
 		/// </summary>
-		public Group() {
+		public Group()
+		{
 			RequiredComponents = Array.Empty<Type>();
 			ExcludedComponents = Array.Empty<Type>();
 		}
 
-		public Group(params Type[] requiredComponents) {
-            if (requiredComponents is null)
-            { throw new ArgumentNullException(nameof(requiredComponents)); }
-
-            RequiredComponents = requiredComponents.ToArray();
+		public Group(params Type[] requiredComponents)
+		{
+			RequiredComponents = requiredComponents?.ToArray() ?? Array.Empty<Type>();
 			ExcludedComponents = Array.Empty<Type>();
 		}
         
 	    public Group(IEnumerable<Type> requiredComponents, IEnumerable<Type> excludedComponents)
 	    {
-            if (requiredComponents is null)
-            { throw new ArgumentNullException(nameof(requiredComponents)); }
-
-            if (excludedComponents is null)
-            { throw new ArgumentNullException(nameof(excludedComponents)); }
-
-            RequiredComponents = requiredComponents.ToArray();
-		    ExcludedComponents = excludedComponents.ToArray();
+            RequiredComponents = requiredComponents?.ToArray() ?? Array.Empty<Type>();
+		    ExcludedComponents = excludedComponents?.ToArray() ?? Array.Empty<Type>();
 	    }
     }
 }
